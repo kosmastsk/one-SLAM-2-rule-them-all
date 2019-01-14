e0052ac8526496c5689318bdbb1c3d8f3540eeb8
kosmastsk <kosmastsk@gmail.com>
2019-01-14 13:12:20 +0200

    Add octomap evaluation package

---
+++ octomap_evaluation/include/octomap_evaluation/octomap_to_pc.h
@@ -0,0 +1,13 @@
+/*   */
+
+#ifndef OCTOMAP_TO_PC_HEADER
+#define OCTOMAP_TO_PC_HEADER
+
+#include <stdio.h>
+
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+
+
+
+#endif
---
+++ octomap_evaluation/src/octomap_to_pc.cpp
@@ -0,0 +1,13 @@
+/* This ROS node implements the concersion of an octomap to Point Cloud format */
+
+#include "octomap_evaluation/octomap_to_pc.h"
+
+/* Main function */
+int main(int argc, char **argv)
+{
+  // Load the octomap file
+
+  //Convert to PC
+
+
+}
