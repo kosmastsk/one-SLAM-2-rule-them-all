be58a92da711a35b1050014f2ec51447c380b68f
nickmour <mourelatosster@gmail.com>
2019-01-17 12:30:20 +0200

    Read both .ot and .bt files from a path specified by the user

--- octomap_evaluation/src/octomap_to_pc.cpp
+++ octomap_evaluation/src/octomap_to_pc.cpp
@@ -17,12 +17,24 @@ Converter::Converter() {
 /******************************/
 Converter::Converter(char *argv[]) {
   // Get the octomap filename from the user
-  std::string path = ros::package::getPath("octomap_evaluation");
-  _filename = path.c_str() + std::string("/maps/") + argv[1];
+  //std::string path = ros::package::getPath("octomap_evaluation");
+  //_filename = path.c_str() + std::string("/maps/") + argv[1];
+  //std::cout << argv[0] << '\n';
+  //std::cout << argv[1] << '\n';
+	std::string _filename=argv[1];
+	/*** Read Octomap ***/
+	if(_filename.substr( _filename.length()-2) == "ot")
+		{
+		  octomap::AbstractOcTree *tree = octomap::AbstractOcTree::read(_filename);
+		  octomap::ColorOcTree *_octree = dynamic_cast<octomap::ColorOcTree *>(tree);
+		  _cloud = octomapToPointCloud(_octree, argv[1]);
+		}else if (_filename.substr( _filename.length()-2) == "bt")
+		{
+			using namespace octomap;
+			octomap::OcTree *_octree = new OcTree(_filename);
+			_cloud = octomapToPointCloud(_octree, argv[1]);
+		}
 
-  _octree = readOctomap(_filename);
-
-  _cloud = octomapToPointCloud(_octree, argv[1]);
 }
 
 /******************************/
@@ -30,15 +42,6 @@ Converter::Converter(char *argv[]) {
 /******************************/
 Converter::~Converter() { ROS_INFO("Class converter has been destroyed\n"); }
 
-/******************************/
-/*        readOctomap         */
-/******************************/
-octomap::ColorOcTree *Converter::readOctomap(std::string filename) {
-  octomap::AbstractOcTree *tree = octomap::AbstractOcTree::read(filename);
-  octomap::ColorOcTree *octree = dynamic_cast<octomap::ColorOcTree *>(tree);
-
-  return octree;
-}
 
 /******************************/
 /*     octomapToPointCloud    */
@@ -46,7 +49,6 @@ octomap::ColorOcTree *Converter::readOctomap(std::string filename) {
 pcl::PointCloud<pcl::PointXYZRGB>
 Converter::octomapToPointCloud(octomap::ColorOcTree *octree, char *filename) {
   pcl::PointCloud<pcl::PointXYZRGB> cloud;
-
   size_t leafs = octree->getNumLeafNodes();
 
   cloud.width = leafs;
@@ -54,7 +56,6 @@ Converter::octomapToPointCloud(octomap::ColorOcTree *octree, char *filename) {
   cloud.is_dense = false; // True if no points are invalid(NaN or Inf).
 
   cloud.points.resize(cloud.width * cloud.height);
-
   int i = 0;
   for (octomap::ColorOcTree::leaf_iterator it = octree->begin_leafs(),
                                            end = octree->end_leafs();
@@ -74,12 +75,51 @@ Converter::octomapToPointCloud(octomap::ColorOcTree *octree, char *filename) {
   }
   ROS_INFO("%d\n", i);
 
-  std::string path = ros::package::getPath("octomap_evaluation");
-  path = path.c_str() + std::string("/maps/") + filename + std::string(".pcd");
+  //std::string path = ros::package::getPath("octomap_evaluation");
+  //path = path.c_str() + std::string("/maps/") + filename + std::string(".pcd");
+  pcl::io::savePCDFileASCII("output.pcd", cloud);
+  //std::cerr << "Saved " << cloud.points.size() << " date points to " << path
+   //         << std::endl;
 
-  pcl::io::savePCDFileASCII(path, cloud);
-  std::cerr << "Saved " << cloud.points.size() << " date points to " << path
-            << std::endl;
+  return cloud;
+}
+/*************************/
+/*octomaptopointcloud non color*/
+pcl::PointCloud<pcl::PointXYZRGB>
+Converter::octomapToPointCloud(octomap::OcTree *octree, char *filename) {
+  pcl::PointCloud<pcl::PointXYZRGB> cloud;
+
+  size_t leafs = octree->getNumLeafNodes();
+
+  cloud.width = leafs;
+  cloud.height = 1;
+  cloud.is_dense = false; // True if no points are invalid(NaN or Inf).
+
+  cloud.points.resize(cloud.width * cloud.height);
+
+  int i = 0;
+  for (octomap::OcTree::leaf_iterator it = octree->begin_leafs(),
+                                           end = octree->end_leafs();
+       it != end; ++it) {
+
+    // Provide the coordinate values from octomap
+    cloud.points[i].x = it.getCoordinate().x();
+    cloud.points[i].y = it.getCoordinate().y();
+    cloud.points[i].z = it.getCoordinate().z();
+    // Color
+    cloud.points[i].r = 0 ;
+    cloud.points[i].g = 132 ;
+    cloud.points[i].b = 132 ;
+    i++;
+  }
+  ROS_INFO("%d\n", i);
+
+  //std::string path = ros::package::getPath("octomap_evaluation");
+  //path = path.c_str() + std::string("/maps/") + filename + std::string(".pcd");
+
+  pcl::io::savePCDFileASCII("output.pcd", cloud);
+  //std::cerr << "Saved " << cloud.points.size() << " date points to " << path
+   //         << std::endl;
 
   return cloud;
 }
